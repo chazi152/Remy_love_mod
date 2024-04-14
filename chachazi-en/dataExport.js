6e97a195330b0462ede9b6fef42f2afcb854a46b
chachazi <2646520070@qq.com>
2024-04-14 10:18:07 +0800

    增加幽灵附身雷米差分

--- chachazi-en/dataExport.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    window.modModLoadController.addLifeTimeCircleHook('Remy_love_mod exportDataZip', {
-        exportDataZip: async (zip) => {
-            // export data that injected to memory
-            zip.file('fenghuang-mods/js/feats', JSON.stringify(window?.DOL?.setup?.feats, undefined, 2));
-            return zip;
-        }
-    })
-})();
